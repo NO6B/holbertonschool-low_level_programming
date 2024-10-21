12535041daf97dd1b57900e410818a9072c2c003
abdel <abdel_azrhar@hotmail.com>
2024-10-21 11:30:34 +0000

    task

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("Size of char: %lu bytes\n", sizeof(char));
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("Size of float: %lu bytes\n", sizeof(float));
+printf("Size of double: %lu bytes\n", sizeof(double));
+printf("Size of pointer: %lu bytes\n", sizeof(void *));
+return 0;
+}
