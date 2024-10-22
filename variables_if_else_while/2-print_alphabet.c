ccff0f19090120e13cf43dc41469d06cce48f7be
abdel <abdel_azrhar@hotmail.com>
2024-10-22 13:19:11 +0000

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char lettre[] = "abcdefghijklmnopqrstuvwxyz\n";
+puts(lettre);
+return (0);
+}
