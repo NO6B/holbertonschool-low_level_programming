c672e290cc336bea3619561c13d94def28f07d27
abdel <abdel_azrhar@hotmail.com>
2024-10-23 16:14:42 +0000

    print alphabet 10*

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
+*/
+int main(int argc, char const *argv[])
+{
+int count=0;
+while (count<10)
+{
+printf("abcdefghijklmnopqrstuvwxyz\n");
+count++;
+}
+return (0);
+}
