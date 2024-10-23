3bd6eb0b35daf904fe77af3f4749daccf7ba9aec
abdel <abdel_azrhar@hotmail.com>
2024-10-23 07:27:18 +0000

    prints the alphabet in lowercase, and then in uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+write(1, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n", 54);
+return (0);
+}
