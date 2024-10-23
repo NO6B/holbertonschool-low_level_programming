1e20f5f936cd031d43138e4dcbf24c467048629d
abdel <abdel_azrhar@hotmail.com>
2024-10-23 14:54:28 +0000

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-/**
-* print_alphabet - Entry point
-* Return:0
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
