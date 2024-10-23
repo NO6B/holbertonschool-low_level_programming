ce83901948f92344fbc7827be50ef05d2af118c9
abdel <abdel_azrhar@hotmail.com>
2024-10-23 08:14:59 +0000

    a

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
-{int i=0;
-for ( i = 0; i < 10; i++)
 {
-printf("%d, ", i);
-}
+write(1, "0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n", 21);
 return (0);
 }
