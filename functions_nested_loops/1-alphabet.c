d05be39a68e85e362137ef06179eac1121c2aa0c
abdel <abdel_azrhar@hotmail.com>
2024-10-23 13:45:44 +0000

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
-    write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
 }
