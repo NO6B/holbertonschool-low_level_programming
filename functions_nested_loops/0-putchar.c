b7024c1d997836941ee1202be77ca2f931ce3a7c
abdel <abdel_azrhar@hotmail.com>
2024-10-23 09:20:20 +0000

    a

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 int main(void)
 {int i=0;
-printf("_putchar\n");
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
 return (0);
 }
