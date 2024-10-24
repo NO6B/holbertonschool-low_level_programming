9c8c54016304be71ad224670793fd58bb745e1f8
abdel <abdel_azrhar@hotmail.com>
2024-10-24 12:50:12 +0000

    printalphabet 10*

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,21 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 /**
- * print_alphabet - utilise printf pour print alphabet
- *              
-*/
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ */
 void print_alphabet_x10(void)
 {
-int count = 0;
-while (count<10)
-  {
-    printf("abcdefghijklmnopqrstuvwxyz\n");
-    count++;
-  }
+    int count = 0;
+
+    while (count < 10)
+    {
+        char i = 'a';
+        while (i <= 'z')
+        {
+            putchar(i);
+            i++;
+        }
+        putchar('\n');
+        count++;
+    }
 }
