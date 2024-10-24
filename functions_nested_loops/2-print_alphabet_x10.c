58f98bdff81f4d3b6fb340404b05758bd5cb257d
abdel <abdel_azrhar@hotmail.com>
2024-10-24 13:07:34 +0000

    updade betty

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_alphabet_x10 - Prints the alphabet 10 times
-*/
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
 void print_alphabet_x10(void)
 {
-int count = 0;
-char alphabet[]="abcdefghijklmnopqrstuvwxyz\n";    
+int count= 0;
+ char alphabet[]="abcdefghijklmnopqrstuvwxyz\n"; 
 while (count < 10)
 {
 write(1 ,alphabet, 27);
-count++;           
-}    
+count++;         
+}
 }
