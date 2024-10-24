d36aed23f8f5da12f9408bc7977c2d32a11be66a
abdel <abdel_azrhar@hotmail.com>
2024-10-24 12:26:38 +0000

    print alphabet 10*

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,10 @@
 */
 void print_alphabet_x10(void)
 {
- int count=0;
+int count = 0;
 while (count<10)
-printf("abcdefghijklmnopqrstuvwxyz\n");
- count++;
+  {
+    printf("abcdefghijklmnopqrstuvwxyz\n");
+    count++;
+  }
 }
