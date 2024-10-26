8f340280b366e7f3dfdb32e901082c4233ad1054
abdel <abdel_azrhar@hotmail.com>
2024-10-26 18:16:46 +0000

    absolute avl

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+int _abs(int r){
+   return abs(r); 
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
+int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-#endif 
+#endif
