ba421ef502ae6ab0a7fc45e79417b23e693ff6c5
abdel <abdel_azrhar@hotmail.com>
2024-10-26 18:23:32 +0000

    absolute value of an integer

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,7 +2,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-
-int _abs(int r){
-   return abs(r); 
+/**
+* _abs - Computes the absolute value of an integer
+* @r: The integer to compute the absolute value of
+* Description: This function takes an integer r and returns its
+* absolute value using the standard library function abs.
+* Return: The absolute value of the integer r
+*/
+int _abs(int r)
+{
+return (abs(r));
 }
