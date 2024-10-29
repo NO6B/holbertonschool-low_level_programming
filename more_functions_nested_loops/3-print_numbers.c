ae44685f74fa6bef28bbc1cac62f1f9ac513d0f8
abdel <abdel_azrhar@hotmail.com>
2024-10-29 08:25:34 +0000

    add Betty documentation style

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,11 @@
 #include <ctype.h>
 #include "main.h"
 void print_numbers(void)
-/**
-* print_numbers - prints the numbers, from 0 to 9,
-* followed by a new line
-*/
+/** * print_numbers - prints the numbers, from 0 to 9, 
+*followed by a new line 
+* * Description: This function prints the numbers from 0 to 9 using a loop 
+* and the _putchar function, then prints a new line. 
+* * Return: void */ 
 {
 int i = 0;
 while (i < 10)
