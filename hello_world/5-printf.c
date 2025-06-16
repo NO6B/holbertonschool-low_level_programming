5782e2791eca49e27b958754099efdea26bf7c6b
abdel <abdel_azrhar@hotmail.com>
2025-06-16 11:16:04 +0200

    C program that prints Use the function printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point of the program
+ * Description: Prints the string"with proper grammar, but the outcome is a pi"
+ * followed by a new line using the puts function.
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
