1b5862484c4a1fef5d4021d2bd6d721309c32f07
abdel <abdel_azrhar@hotmail.com>
2025-06-16 18:53:13 +0200

    program that prints all the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+for (i = 'a'; i <= 'f'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
