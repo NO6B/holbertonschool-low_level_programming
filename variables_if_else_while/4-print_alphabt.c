877242d857bada75763a8add709806028b1143aa
abdel <abdel_azrhar@hotmail.com>
2025-06-16 15:15:17 +0200

    Print all the letters except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'd'; alphabet++)
+putchar(alphabet);
+for (alphabet = 'f'; alphabet <= 'p'; alphabet++)
+putchar(alphabet);
+for (alphabet = 'r'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
