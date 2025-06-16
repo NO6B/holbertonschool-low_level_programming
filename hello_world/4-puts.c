7756be5214403c5bf9fe98b603e779605153787c
abdel <abdel_azrhar@hotmail.com>
2025-06-16 11:04:26 +0200

    C program Use the function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,11 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "Programming is like building a multilingual puzzle"
+ * followed by a new line using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 int main(void)
