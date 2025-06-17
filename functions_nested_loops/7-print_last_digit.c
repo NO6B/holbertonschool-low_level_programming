f0bf00132fb110be793d9590ca236499f40d0a52
abdel <abdel_azrhar@hotmail.com>
2025-06-17 22:42:56 +0200

    update betty doc

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 /**
  * print_last_digit - print the last digit.
- * @n: The integer to compute the absolute value of.
+ * @r: The integer to compute the absolute value of.
  *
- * Return: The absolute value of the integer @n.
+ * Return: The absolute value of the integer @r.
  */
 #include "main.h"
-int print_last_digit(int n)
+int print_last_digit(int r)
 {
-return (10 % n);
+return (r % 10);
 }
