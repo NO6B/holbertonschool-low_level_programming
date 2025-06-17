74d57c9014e006a23d0ec1f6bf3756eba17ee4d8
abdel <abdel_azrhar@hotmail.com>
2025-06-17 20:49:36 +0200

    Returns 1 if c is a letter, lowercase or uppercase

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - Checks if a character is a lowercase letter
+ * @c: The character to check (ASCII code)
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
