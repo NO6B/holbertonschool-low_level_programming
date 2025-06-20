12f2c5a2391efe08ed3dd7d3ff20414d51f340c8
abdel <abdel_azrhar@hotmail.com>
2025-06-20 09:33:45 +0200

    updte file

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,7 +9,6 @@
 void times_table(void)
 {
 int x, y;
-int k, j;
 for (y = 0; y < 10; y++)
 {
 for (x = 0; x < 10; x++)
@@ -20,14 +19,4 @@ printf(",  ");
 }
 printf("\n");  
 } 
-for (k = 0; k < 10; k++)
-{
-for (j = 0; j < 10; j++)
-{
-printf("%d", k * j);
-if (j < 9)
-printf(",..");
-}
-printf("\n");
-}
 }
