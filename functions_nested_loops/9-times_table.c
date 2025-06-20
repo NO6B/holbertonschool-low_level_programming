667eb6df2e1e3dbc625e4298d07151c3bf67052d
abdel <abdel_azrhar@hotmail.com>
2025-06-20 10:41:26 +0200

    add condition $

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * times_table - print .
  *
@@ -7,25 +6,21 @@
  */
 void times_table(void)
 {
-    int x, y, r;
-
-    for (y = 0; y < 10; y++)
-    {
-        for (x = 0; x < 10; x++)
-        {
-            r = x * y;
-
-            if (x != 0)
-                _putchar(',');
-            _putchar(' ');
-
-            if (r < 10)
-                _putchar(' ');
-            else
-                _putchar((r / 10) + '0');
-
-            _putchar((r % 10) + '0');
-        }
-        _putchar('\n');
-    }
+int x, y, r;
+for (y = 0; y < 10; y++)
+{
+for (x = 0; x < 10; x++)
+{
+r = x * y;
+if (x != 0)
+_putchar(',');
+_putchar(' ');
+if (r < 10)
+_putchar(' ');
+else
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
+}
+_putchar('\n');
+}
 }
