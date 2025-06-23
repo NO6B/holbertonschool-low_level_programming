ba6140f4bd3f05bca51fa17664edeb55909eafaf
NO6B <abdel_azrhar@hotmail.com>
2025-06-23 15:15:19 +0200

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * print_line - Point d'entrée du programme
- *
- * Return: Toujours 0 (Succès)
+ * print_line - Draws a straight line using underscores.
+ * @n: The number of '_' characters to print.
+ * Description: Prints a line of underscores followed by a newline.
+ * If n is 0 or less, only the newline is printed.
+ * Return: Nothing.
  */
 void print_line(int n)
 {
