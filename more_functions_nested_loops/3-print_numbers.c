7e29b25016c38c837b98f5a1b98e23055a8fc8b2
abdel <abdel_azrhar@hotmail.com>
2025-06-23 12:48:56 +0200

    update betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - Prints digits from 0 to 9
  *
  * Return: void
  */
-void print_numbers(void);void print_numbers(void)
+void print_numbers(void)
 {
-for (int i = 0; i < 10; i++)
+int i;
+for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
 }
+putchar('\n');
 }
