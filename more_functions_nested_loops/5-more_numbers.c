da82677aaf914282444274b5be562e7825e0e936
Abdel <abdel_azrhar@hotmail.com>
2025-06-23 14:43:05 +0200

    update

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@ void more_numbers(void)
 {
 int y;
 int x;
-for (y = 0; y < 10; y++)
-{
+    for (y = 0; y < 10; y++)
+    {
 for (x = 0; x < 15; x++)
+        {
+            if (x > 9)
 {
-if (x > 9)
-{
-_putchar((x / 10) + '0');
+                _putchar((x / 10) + '0');
 }
-_putchar((x % 10) + '0');
-}
-_putchar('\n');
-}
+            _putchar((x % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
