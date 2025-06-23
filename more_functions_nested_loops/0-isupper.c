87d5097fa20443a750ba967cb7cdb31d3cd0d7d6
NO6B <abdel_azrhar@hotmail.com>
2025-06-23 10:26:44 +0200

    Update 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int _isupper(int c)
 {
-    if (isupper(c))
-    {
-        return 1;
-    }
-    else
-    return 0;
+if (isupper(c))
+{
+return 1;
+}
+else
+return 0;
 }
