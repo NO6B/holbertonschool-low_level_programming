7b52c4a93479f925c73d7fbe7cc78a83f6635f60
NO6B <abdel_azrhar@hotmail.com>
2025-06-23 12:37:02 +0200

    Update 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,13 @@
 #include "main.h"
-
-
+/**
+ * print_numbers - Prints digits from 0 to 9
+ *
+ * Return: void
+ */
 void print_numbers(void);void print_numbers(void)
-{ 
-    for (int i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }  
+{
+for (int i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
 }
