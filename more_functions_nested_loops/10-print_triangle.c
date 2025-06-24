29cff55a0f9dd72df2452a10b2f43d1911e37676
abdel <abdel_azrhar@hotmail.com>
2025-06-25 00:07:58 +0200

    betty update

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -4,23 +4,21 @@
  * @size: Height of the triangle
  */
 void print_triangle(int size)
-{ 
-   int i, j;
-    if (size <= 0)
-    _putchar('\n');
-
-    for (i = 0; i < size; i++)
-    {
-    int k;
-    for (k = size; k > i; k--)
-    {
-    _putchar(' ');
-    }
-    for (j = 0; j <= i; j++)
-    {
-    _putchar('#');
-    }      
-    _putchar('\n');
-
-  }
+{
+int i, j;
+if (size <= 0)
+_putchar('\n');
+for (i = 0; i < size; i++)
+{
+int k;
+for (k = size; k > i; k--)
+{
+_putchar(' ');
+}
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
