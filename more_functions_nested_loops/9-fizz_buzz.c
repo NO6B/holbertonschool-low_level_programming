328a2799ff2c368e7b20d3042e9efd72cfee3889
NO6B <abdel_azrhar@hotmail.com>
2025-06-24 11:10:47 +0200

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,19 @@ int main(int argc, char const *argv[])
 {
 for (int i = 0; i <= 100; i++)
 {
-            printf("%d \n",i);
-            if (i %3 == 0)
-            {
-            printf("Fizz") ;
-            }
-            else if (i %5 == 0)
-            {
-                printf("Buzz");
-            }
-            else if (i %3 ==0 || i %5 == 0)
-            {
-                printf("FizzBuzz");
-            }
-        }
-        
-        return 0;
-    }
-
+printf("%d \n",i);
+if (i %3 == 0)
+{
+printf("Fizz");
+}
+else if (i %5 == 0)
+{
+printf("Buzz");
+}
+else if (i %3 ==0 || i %5 == 0)
+{
+printf("FizzBuzz");
+}
+}     
+return 0;
+}
