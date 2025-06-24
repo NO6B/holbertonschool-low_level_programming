f6ef03c416c3f90e32b04f86f2daa0e3bc576ec1
Abdel <abdel_azrhar@hotmail.com>
2025-06-24 12:44:20 +0200

    add file

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
     for (i = 0; i <= 100; i++)
     {
         if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz\n");
+            printf("FizzBuzz");
         else if (i % 3 == 0)
             printf("Fizz");
         else if (i % 5 == 0)
