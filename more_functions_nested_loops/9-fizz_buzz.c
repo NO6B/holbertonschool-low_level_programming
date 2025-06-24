84e05da2af4397428b43110a12263dd68a75ed75
NO6B <abdel_azrhar@hotmail.com>
2025-06-24 16:54:41 +0200

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,10 @@ int main(void)
             printf("Buzz");
         else
             printf("%d", i);
-        
-
         if (i < 100)
         {
             printf(" ");
-        }
-        
+        } 
     }
     printf("\n");
     return 0;
