96639a57e8ed875c0efe5b1718475ce153073cb0
Abdel <abdel_azrhar@hotmail.com>
2025-06-24 11:02:32 +0200

    create files

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
-    int main(int argc, char const *argv[])
-    {
-        for (int i = 0; i <= 100; i++)
-        {
+/**
+ * main - Prints 0 to 100 with FizzBuzz logic
+ *
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+for (int i = 0; i <= 100; i++)
+{
             printf("%d \n",i);
             if (i %3 == 0)
             {
