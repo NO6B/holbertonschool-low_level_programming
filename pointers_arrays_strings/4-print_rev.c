2cee24e4bce5236ca7c2ea4c935ea8c6c306dc6a
NO6B <abdel_azrhar@hotmail.com>
2025-06-25 20:50:36 +0200

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
+/**
+ * reverse_string - Inverse une chaîne de caractères
+ * @s: Chaine a inverser
+ * Return: Rien
+ */
 void print_rev(char *s)
 {
-    int i = 0;
-    int j;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    for (j = i; j >= 0; j--)
-    {
-        _putchar(s[j]);
-    }
-    _putchar('\n');
+int i = 0;
+int j;
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = i; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
 }
