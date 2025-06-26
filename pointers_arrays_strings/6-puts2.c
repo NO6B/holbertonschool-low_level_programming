a5b36313e4b909ad1d77c38295c1906b0b407c07
Abdel <abdel_azrhar@hotmail.com>
2025-06-26 11:51:58 +0200

    .

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Pointer to the string
+ */
+void puts2(char *str){
+    int i =0;
+    int j;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    i--;
+    for (j = 0; j < i; j+=2)
+    {
+        _putchar(str[j]);
+    }
+    _putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ void _puts(char *str);
 int _putchar(int c);
 void print_rev(char *s);
 void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+void puts2(char *str);
