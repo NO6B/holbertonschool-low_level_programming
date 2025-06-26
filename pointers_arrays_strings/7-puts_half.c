827b963c9c9804c7b59fac76326a3b128617a8e9
NO6B <abdel_azrhar@hotmail.com>
2025-06-26 13:41:44 +0200

    Update 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,12 @@ void puts_half(char *str)
 int i = 0;
 int j;
 while (str[i] != '\0')
-
-    i++;
-
-j=i/2;
-while (str[j]!= '\0')
+i++;
+j = i/2;
+while (str[j] != '\0')
 {
-    _putchar(str[j]);
-    j++;
+_putchar(str[j]);
+j++;
 }
 _putchar('\n');
 }
