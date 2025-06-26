1c0a285dbc73d8bd29730fe1277d190808444d25
Abdel <abdel_azrhar@hotmail.com>
2025-06-26 13:40:09 +0200

    .

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - Prints the second half of a string
+ * @str: Pointer to the string
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int j;
+while (str[i] != '\0')
+
+    i++;
+
+j=i/2;
+while (str[j]!= '\0')
+{
+    _putchar(str[j]);
+    j++;
+}
+_putchar('\n');
+}
