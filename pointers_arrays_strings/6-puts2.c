29ade6415836fc97bb15dcb8ba0ef4488ebe3df5
NO6B <abdel_azrhar@hotmail.com>
2025-06-26 11:53:07 +0200

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,17 +3,18 @@
  * puts2 - Prints every other character of a string
  * @str: Pointer to the string
  */
-void puts2(char *str){
-    int i =0;
-    int j;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    i--;
-    for (j = 0; j < i; j+=2)
-    {
-        _putchar(str[j]);
-    }
-    _putchar('\n');
+void puts2(char *str)
+{
+int i =0;
+int j;
+while (str[i] != '\0')
+{
+i++;
+}
+i--;
+for (j = 0; j < i; j+=2)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
 }
