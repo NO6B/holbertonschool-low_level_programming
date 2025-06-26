1db9070998358a138ab3c5c5fc692286cd3721cb
NO6B <abdel_azrhar@hotmail.com>
2025-06-26 11:26:53 +0200

    Update 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,23 +5,19 @@
  */
 void rev_string(char *s)
 {
-    int i = 0, j = 0;
-    char tmp;
-
-    
-    while (s[i] != '\0')
-    {
-        i++;
-    
-    }
-    i--; 
-    while (j < i)
-    {
-        tmp = s[j];
-        s[j] = s[i];
-        s[i] = tmp;
-        j++;
-        i--;
-    }
+int i = 0, j = 0;
+char tmp;
+while (s[i] != '\0')
+{
+i++;
 }
-
+i--;
+while (j < i)
+{
+tmp = s[j];
+s[j] = s[i];
+s[i] = tmp;
+j++;
+i--;
+}
+}
