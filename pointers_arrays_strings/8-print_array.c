a43c949629329c5bf2ee9d8c074c48a2bbf40cfe
Abdel <abdel_azrhar@hotmail.com>
2025-06-26 21:43:17 +0200

    prints n elements of an array of integers, followed

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,10 @@ void print_array(int *a, int n)
   for (i = 0; n > i ; i++)
   { 
     printf("%d", a[i]);
-    if (i != n - 1)
+    if (i != n -1)
     {
-    printf(", ");
+    printf(",");
+    printf(" ");
     }
   }
 }
