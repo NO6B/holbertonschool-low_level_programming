c5b2fe9100adbc35a2d74626e272606c993eadf8
NO6B <abdel_azrhar@hotmail.com>
2025-06-27 13:35:24 +0200

    Update 0-strcat.c

--- 0-pointers_arrays_strings/0-strcat.c
+++ 0-pointers_arrays_strings/0-strcat.c
@@ -7,23 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
-
-    
-    while (dest[j] != '\0')
-    {
-        j++;
-    }
-
-   
-    while (src[i] != '\0')
-    {
-        dest[j] = src[i];
-        j++;
-        i++;
-    }
-    dest[j] = '\0';
-
-    return (dest);
+int i = 0;
+int j = 0;
+while (dest[j] != '\0')
+{
+j++;
+}
+while (src[i] != '\0')
+{
+dest[j] = src[i];
+j++;
+i++;
+}
+dest[j] = '\0';
+return (dest);
 }
