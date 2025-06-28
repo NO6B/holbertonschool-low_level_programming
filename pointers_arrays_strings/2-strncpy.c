90e1695f29c0a33be6c52987480300334a32fd28
NO6B <abdel_azrhar@hotmail.com>
2025-06-28 18:06:33 +0200

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,7 @@ char *_strncpy(char *dest, char *src, int n)
 {int i;
 for (i = 0; src[i] != '\0' && i < n; i++) 
 {
-dest[i] = src[i];/*les premier element de src est copier a la place du premier element de dest */
+dest[i] = src[i];/*le premiers element de src est copier a la place du premier element de dest */
 for (; i < n; i++)
 {
 dest[i] = '\0';
