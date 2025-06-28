1c7f080c814158f5e4764a979c6190dd1a873e35
NO6B <abdel_azrhar@hotmail.com>
2025-06-28 18:37:50 +0200

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,15 +2,15 @@ char *_strncpy(char *dest, char *src, int n)
 /**
  * _strncpy - Copies function
  * @dest: Destination
- * @src: Source 
- * @n: Maximum number 
+ * @src: Source
+ * @n: Maximum number
  * Return: Pointer to the destination buffer
  */
 {
 int i;
-for (i = 0; src[i] != '\0' && i < n; i++) 
+for (i = 0; src[i] != '\0' && i < n; i++)
 {
-dest[i] = src[i];/*le premier element de src est copier a la place du premier element de dest */
+dest[i] = src[i];/*le premier element src=premier element de dest*/
 }
 for (; i < n; i++)
 {
