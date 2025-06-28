11e666109ec7042cd2cc665e235bf9aead238994
aBDEL <abdel_azrhar@hotmail.com>
2025-06-28 18:34:00 +0200

    function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@ char *_strncpy(char *dest, char *src, int n)
  * Return: Pointer to the destination buffer
  */
 {   int i;
-    for (i = 0; src[i] != '\0' && i < n; i++) /*src = phrase dest = * */
+    for (i = 0; src[i] != '\0' && i < n; i++) /*src = phrase et dest = * */
     {
         dest[i] = src[i]; /*les premier element de src est copier a la place du premier element de dest */
     }
@@ -17,4 +17,3 @@ char *_strncpy(char *dest, char *src, int n)
     }
 return (dest);
 }
- 
