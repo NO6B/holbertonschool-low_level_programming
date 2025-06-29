fda4375db8e6960b286f953e54ac3fad25404dee
Abdel <abdel_azrhar@hotmail.com>
2025-06-29 16:18:15 +0200

    betty com

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *string_toupper - transforme lowercase letters of a string to uppercase.
+ * @str: Pointeur vers la chaîne de caractères
+ * Return: str
+ */
+char *string_toupper(char *str)
+{   
+    char min[] ="abcdefghijklmnopqrstuvwxyz";
+    int i;
+    int j;
+    char maj[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0;maj[j] != '\0'; j++)
+        {
+            if (str[i] == min[j])
+            {
+                str[i] = maj[j];
+            }
+            else if (str[i] == maj[j])
+            {
+                str[i] = maj[j];
+            }
+            
+            
+        }
+        
+    }
+ return (str);  
+}
