4914bd01f63536361e997d94812d6b3721b82d13
NO6B <abdel_azrhar@hotmail.com>
2025-06-29 16:20:13 +0200

    Update 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -5,27 +5,24 @@
  * Return: str
  */
 char *string_toupper(char *str)
-{   
-    char min[] ="abcdefghijklmnopqrstuvwxyz";
-    int i;
-    int j;
-    char maj[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0;maj[j] != '\0'; j++)
-        {
-            if (str[i] == min[j])
-            {
-                str[i] = maj[j];
-            }
-            else if (str[i] == maj[j])
-            {
-                str[i] = maj[j];
-            }
-            
-            
-        }
-        
-    }
- return (str);  
+{
+char min[] ="abcdefghijklmnopqrstuvwxyz";
+int i;
+int j;
+char maj[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0;maj[j] != '\0'; j++)
+{
+if (str[i] == min[j])
+{
+str[i] = maj[j];
+}
+else if (str[i] == maj[j])
+{
+str[i] = maj[j];
+}
+} 
+}
+return (str);
 }
