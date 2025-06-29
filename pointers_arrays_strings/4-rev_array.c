ea58c291ad8bc1734486b72e128c821ee5de22fc
NO6B <abdel_azrhar@hotmail.com>
2025-06-29 22:48:06 +0200

    Update 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -6,16 +6,15 @@
  */
 void reverse_array(int *a, int n)
 {
-    int tmp;
-    int i = 0;
-    int j = n - 1;
-
-    while (i < j) {
-        
-        tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-        i++;
-        j--;
-    }
+int tmp;
+int i = 0;
+int j = n - 1;
+while (i < j)
+{        
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+i++;
+j--;
+}
 }
