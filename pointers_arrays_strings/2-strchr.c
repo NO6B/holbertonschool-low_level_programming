eea059a7595a8c7a64838dac3c059cd280fd9ce1
NO6B <abdel_azrhar@hotmail.com>
2025-06-30 17:33:45 +0200

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _strchr - Locate first occurrence of char in string
  * @s: String to search
@@ -7,15 +6,14 @@
  * Return: Pointer to first occurrence or NULL
  */
 char *_strchr(char *s, char c)
-{   
-    int i;
-
-    for (i = 0; s[i] != '\0'; i++)
-    {   
-         if (s[i] == c)
-         {
-            return &s[i];    
-         }
-    }
-    return (s);   
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return &s[i];
+}
+}
+return (s);
 }
