bdfe673fc58d2eaf7cbc47d9edb3613576a5ae8e
Abdel <abdel_azrhar@hotmail.com>
2025-07-01 14:15:13 +0200

    .

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: String to search
+ * @accept: ta
+ * Return: i
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i = 0, j;
+
+    while (s[i] != '\0')
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+            if (s[i] == accept[j])
+                break;
+
+        if (!accept[j])  /* Si on a parcouru tout accept sans trouver s[i]*/
+            return i;
+
+        i++;
+    }
+    return i;
+}
