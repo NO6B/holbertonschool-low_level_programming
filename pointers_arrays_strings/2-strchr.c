c5b7f407dbca3909104b0b68b944a3ad73b4ba38
NO6B <abdel_azrhar@hotmail.com>
2025-07-01 11:34:26 +0200

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,13 @@
  * Return: Pointer to first occurrence or NULL
  */
 char *_strchr(char *s, char c)
-{   
-    int i;
-
-    for (i = 0; s[i] != '\0'; i++)
-    {   
-        if (s[i] == c)
-            return &s[i];
-              
-    }        
-    
-    if (c == '\0') 
-        return NULL;
-       
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+return &s[i];
+}
+if (c == '\0')
+return NULL;
 }
