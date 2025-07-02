a427ca0d1807f7699b8b9e6316951c121e71f6e6
NO6B <abdel_azrhar@hotmail.com>
2025-07-02 12:38:32 +0200

    Update 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -18,10 +18,10 @@ for (j = 0; needle[j] != '\0'; j++)
 if (haystack[i + j] != needle[j])
 break;
 }
-if (needle[j] =='\0')
+if (needle[j] == '\0')
 {
 return(&haystack[i]);
 }
 }
-return (NULL);
+return(NULL);
 }
