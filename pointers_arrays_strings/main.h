cd003031f96690b82c8205a2119d667bdecad48f
aBDEL <abdel_azrhar@hotmail.com>
2025-07-02 20:19:33 +0200

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,3 +20,4 @@ char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
