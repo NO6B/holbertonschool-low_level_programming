9708174a1beea181d64e81042151d084d8703446
NO6B <abdel_azrhar@hotmail.com>
2025-07-02 12:37:44 +0200

    Update 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
-    int j;
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-        }
-        if (needle[j] =='\0')
-        {
-            return(&haystack[i]);
-        }
-          
-    }
-    return (NULL);
+int i;
+int j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+break;
+}
+if (needle[j] =='\0')
+{
+return(&haystack[i]);
+}
+}
+return (NULL);
 }
