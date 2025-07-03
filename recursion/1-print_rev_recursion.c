1d7593787bcd8a442dfd434614589c71b1b00df8
Abdel <abdel_azrhar@hotmail.com>
2025-07-03 14:21:32 +0200

    .

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - prints a string using recursion
+ * _print_rev_recursion - prints a string using recursion
  * @s: string to print
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {   
     if (*s == '\0')
     {
-    _putchar('\n');
     return;
     }
     else
+    _print_rev_recursion(s + 1); 
     _putchar(*s);
-    _puts_recursion(s + 1);
 }
