eafb17391aa169110dfb3d93b96ffd4ebfea0b22
Abdel <abdel_azrhar@hotmail.com>
2025-07-03 11:39:31 +0200

    add .

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,19 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_chessboard - Prints a 8x8 chessboard.
- * @a: 2D array of characters (8 columns).
+ * print_chessboard - Affiche un échiquier 8x8.
+ * @a: Tableau 2D de caractères (8 colonnes).
  */
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-for (i = 0; i < 8; i++)
-{
-for (j = 0; j < 8; j++)
-{
-_putchar(a[i][j]);
-_putchar(' ');
-}
-_putchar('\n');
-}
+    int i, j;
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            _putchar(a[i][j]);
+        }
+        _putchar('\n');
+    }
 }
---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts_recursion(char *s)
+{
+    int i;
+    for(i=0;s[i] != '\0';i++)
+    _putchar(s[i]);
+}
