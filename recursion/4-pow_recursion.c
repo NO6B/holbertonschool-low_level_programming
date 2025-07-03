8718542aea4ae62d1e28a152f418cf03d5442432
NO6B <abdel_azrhar@hotmail.com>
2025-07-03 17:57:10 +0200

    Update 4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
  * _pow_recursion - returns the factorial of a given number
- * @x: the number 
+ * @x: the number
  * @y: the number
  * Return: -1 if y < 0
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-    return (-1);
-    }
-    if (y == 0)
-    return (1);
-    
-   return (x * _pow_recursion(x, y - 1));
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+return (1); 
+return (x * _pow_recursion(x, y - 1));
 }
