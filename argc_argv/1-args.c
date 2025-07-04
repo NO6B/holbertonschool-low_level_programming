01eb8591597890ab17cfaf757b7e8b8fd053a9b7
NO6B <abdel_azrhar@hotmail.com>
2025-07-04 10:32:14 +0200

    Update 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,11 +6,9 @@
  * Return: 0 on success
  */
 #include <stdio.h>
-
 int main(int argc, char *argv[])
 {
-    (void)argv;
-    printf("%d\n", argc - 1);
-    return 0;
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
 }
-
