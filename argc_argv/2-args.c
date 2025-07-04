850cdb667de7fae18439383f7ebbce298e4effb0
NO6B <abdel_azrhar@hotmail.com>
2025-07-04 10:38:40 +0200

    Update 2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -8,10 +8,10 @@ int main(int argc, char *argv[])
  * Return: 0 on success
  */
 {
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-    return 0;
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
