24c87cb3d15b9197780259e3968aa376fff6e330
aBDEL <abdel_azrhar@hotmail.com>
2025-07-04 10:27:57 +0200

    add

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    printf("%d", argc);
+    return 0;
+}
