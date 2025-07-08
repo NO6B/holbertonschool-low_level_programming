8c9d134af896db89f44435c8084f49dbfc929fbf
NO6B <abdel_azrhar@hotmail.com>
2025-07-08 19:56:36 +0200

    Update 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,28 +8,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i = 0, j = 0, k, l;
-    char *tab;
-
-    while (s1[i] != '\0')
-        i++;  /*i = longueur de s1*/
-
-    while (s2[j] != '\0')
-        j++;  /*j = longueur de s2 */
-
-    tab = malloc((i + j + 1) * sizeof(char));
-    if (tab == NULL)
-        return (NULL);
-
-    /*Copier s1 dans tab*/
-    for (k = 0; k < i; k++)
-        tab[k] = s1[k];
-
-    /*Copier s2 dans tab à la suite*/
-    for (l = 0; l < j; l++)
-        tab[k + l] = s2[l];
-
-    tab[k + l] = '\0'; /*Terminer la chaîne*/
-
-    return (tab);
+int i = 0, j = 0, k, l;
+char *tab;
+while (s1[i] != '\0')
+i++;  /*i = longueur de s1*/
+while (s2[j] != '\0')
+j++;  /*j = longueur de s2 */
+tab = malloc((i + j + 1) * sizeof(char));
+if (tab == NULL)
+return (NULL);
+/*Copier s1 dans tab*/
+for (k = 0; k < i; k++)
+tab[k] = s1[k];
+/*Copier s2 dans tab à la suite*/
+for (l = 0; l < j; l++)
+tab[k + l] = s2[l];
+tab[k + l] = '\0'; /*Terminer la chaîne*/
+return (tab);
 }
