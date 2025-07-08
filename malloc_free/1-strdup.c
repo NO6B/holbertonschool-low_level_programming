a7a24dd85c1b45ae3a56599b78abf882fd730e57
Abdel <abdel_azrhar@hotmail.com>
2025-07-08 12:35:59 +0200

    .

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - give memorie to tab
+ * @str: tab
+ * Return: if size = NULL , or coppy
+ */
+char *_strdup(char *str)
+{
+    int i;
+    int j;
+    char *copy;
+
+    if (str == NULL)
+        return (NULL);
+
+    /* longueur de la chaîne (j)*/
+    for (j = 0; str[j] != '\0'; j++)
+    {}
+
+    /*Allocation mémoire pour la copie (j + 1 pour le '\0')*/
+    copy = malloc((j + 1) * sizeof(char));
+    if (copy == NULL)
+        return (NULL);
+
+    for (i = 0; i < j; i++)
+        copy[i] = str[i];
+
+    return (copy);
+}
