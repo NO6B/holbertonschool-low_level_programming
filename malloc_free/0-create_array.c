71feb0f84d089d0410965aa8a3c8cff9645111c0
Abdel <abdel_azrhar@hotmail.com>
2025-07-08 09:43:56 +0200

    add betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,20 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * create_array - give memorie to tab
+ * @c: Memory area
+ * @size: size of tab
+ * Return: if size = 0 NULL , or array
+ */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
+    char *tab;
     unsigned int i;
 
     if (size == 0)
         return (NULL);
 
-    array = malloc(size * sizeof(char));
-    if (array == NULL)
+    tab = malloc(size * sizeof(char));
+    if (tab == NULL)
         return (NULL);
 
     for (i = 0; i < size; i++)
-        array[i] = c;
+        tab[i] = c;
 
-    return (array);
+    return (tab);
 }
