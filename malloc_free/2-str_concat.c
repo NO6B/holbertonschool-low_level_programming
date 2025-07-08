c3d8460438c5d817e5a09cc528258883d52033d7
aBDEL <abdel_azrhar@hotmail.com>
2025-07-08 19:55:15 +0200

    add

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,21 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ * Return: Pointer to new string, or NULL.
+ */
 char *str_concat(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
-char tab;
+    int i = 0, j = 0, k, l;
+    char *tab;
 
+    while (s1[i] != '\0')
+        i++;  /*i = longueur de s1*/
 
-while (s2[j] != '\0')
-{
-j++;
-}
-while (s1[i] != '\0')
-{
-s2[j] = s1[i];
-j++;
-i++;
-}
-tab = malloc((s2 + 1) * sizeof(char)); 
+    while (s2[j] != '\0')
+        j++;  /*j = longueur de s2 */
+
+    tab = malloc((i + j + 1) * sizeof(char));
+    if (tab == NULL)
+        return (NULL);
+
+    /*Copier s1 dans tab*/
+    for (k = 0; k < i; k++)
+        tab[k] = s1[k];
+
+    /*Copier s2 dans tab à la suite*/
+    for (l = 0; l < j; l++)
+        tab[k + l] = s2[l];
+
+    tab[k + l] = '\0'; /*Terminer la chaîne*/
+
+    return (tab);
 }
