934ff66db7f2ceed32f90e3908f86b12c9d631ce
NO6B <abdel_azrhar@hotmail.com>
2025-07-10 01:34:46 +0200

    Update 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,21 +8,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    int *tab;
-    unsigned int i;
-    if (nmemb ==0 || size == 0)
-    {
-       return (NULL);
-    }
-    
-    tab = malloc(nmemb * size);
-    if (tab == NULL)
-        return (NULL);
-    
-    /*initialiser manuellement chaque element du tableau à 0 */
-    for (i = 0; i < nmemb; i++)
-        tab[i] = 0;
-
-    return (tab);
-
+int *tab;
+unsigned int i;
+if (nmemb ==0 || size == 0)
+{
+return (NULL);
+}
+tab = malloc(nmemb * size);
+if (tab == NULL)
+return (NULL);
+/*initialiser manuellement chaque element du tableau à 0 */
+for (i = 0; i < nmemb; i++)
+tab[i] = 0;
+return (tab);
 }
