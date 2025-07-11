f8550547977382fd082292d1c841932023d576ee
NO6B <abdel_azrhar@hotmail.com>
2025-07-11 15:22:09 +0200

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,52 +3,48 @@
 #include <string.h>
 int len(char *src)
 {
-    int i = 0; 
-    while (src[i] != '\0')
-    {
-        i++;
-    }
-   return (i + 1); 
+int i = 0;
+while (src[i] != '\0')
+{
+i++;
 }
-
+return (i + 1);
+}
 char *ctr(char *dest, char *src)
 {
-    int i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
 }
-
+dest[i] = '\0';
+return (dest);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *new_dog;
-    char * new_name;
-    char * new_owner;
-    new_name = malloc(len(name));
-    if (new_name == NULL)
-    {
-        return (NULL);
-    }
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-    {
-        return (NULL);
-    }
-    new_owner=malloc(len(owner));
-    if (new_owner == NULL)
-    {
-        return (NULL);
-    }   
-    ctr(new_name, name);
-
-    ctr(new_owner, owner);
-
-    new_dog ->name = new_name;
-    new_dog ->owner = new_owner;
-    new_dog ->age = age;
-    return (new_dog);
+dog_t *new_dog;
+char * new_name;
+char * new_owner;
+new_name = malloc(len(name));
+if (new_name == NULL)
+{
+return (NULL);
+}
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+{
+return (NULL);
+}
+new_owner=malloc(len(owner));
+if (new_owner == NULL)
+{
+return (NULL);
+}
+ctr(new_name, name);
+ctr(new_owner, owner);
+new_dog ->name = new_name;
+new_dog ->owner = new_owner;
+new_dog ->age = age;
+return (new_dog);
 }
