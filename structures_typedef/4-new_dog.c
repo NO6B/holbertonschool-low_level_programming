dd955be9f814fb6699535440e646aa5d22e4160a
NO6B <abdel_azrhar@hotmail.com>
2025-07-11 18:45:14 +0200

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,19 +5,18 @@
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
- *
  * Return: Pointer to the allocated dog_t, or NULL on failure
  */
 int len(char *src)
 {
-    int i = 0; 
-    while (src[i] != '\0')
-    {
-        i++;
-    }
-    return (i + 1); 
+int i = 0;
+while (src[i] != '\0')
+{
+i++;
 }
-    char *ctr(char *dest, char *src)
+return (i + 1);
+}
+char *ctr(char *dest, char *src)
 {
 int i = 0;
 while (src[i] != '\0')
@@ -30,34 +29,34 @@ return (dest);
 }
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;/*ajout dun nouveau pointeur a la structure dog_t*/
-    char * new_name;
-    char * new_owner;
-    new_name = malloc(len(name));
-    if (new_name == NULL)
-    {   
-        return (NULL);
-    }
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-    {
-        free(new_name);
-        return (NULL);
-    }
-    new_owner=malloc(len(owner));
-    if (new_owner == NULL)
-    {
-        free(new_dog);
-        free(new_name);
-        return (NULL);
-    }   
-    ctr(new_name, name);
-    ctr(new_owner, owner);
+dog_t *new_dog;/*ajout dun nouveau pointeur a la structure dog_t*/
+char * new_name;
+char * new_owner;
+new_name = malloc(len(name));
+if (new_name == NULL)
+{
+return (NULL);
+}
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+{
+free(new_name);
+return (NULL);
+}
+new_owner=malloc(len(owner));
+if (new_owner == NULL)
+{
+free(new_dog);
+free(new_name);
+return (NULL);
+}   
+ctr(new_name, name);
+ctr(new_owner, owner);
 /*new_dog est un pointeur qui permet
- d’assigner à la structure dog_t l’adresse de
+d’assigner à la structure dog_t l’adresse de
 new_name dans son champ name*/
-    new_dog ->name = new_name;
-    new_dog ->owner = new_owner;
-    new_dog ->age = age;
-    return (new_dog);
+new_dog ->name = new_name;
+new_dog ->owner = new_owner;
+new_dog ->age = age;
+return (new_dog);
 }
