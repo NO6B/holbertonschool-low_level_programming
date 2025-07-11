ef2507ae3ac1330c20d58bcb80b178317059c228
aBDEL <abdel_azrhar@hotmail.com>
2025-07-11 15:14:11 +0200

    update fonction

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,54 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
-/**
- * struct dog - Structure that defines a dog
- * @name: Pointer to the name of the dog
- * @age: Age of the dog (in years)
- * @owner: Pointer to the name of the dog's owner
- *
- * Description: This structure stores information about a dog,
- * including its name, age, and the name of its owner.
- */
+int len(char *src)
+{
+    int i = 0; 
+    while (src[i] != '\0')
+    {
+        i++;
+    }
+   return (i + 1); 
+}
+
+char *ctr(char *dest, char *src)
+{
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
   dog_t *new_dog;
     char * new_name;
     char * new_owner;
-    new_name = malloc(strlen(name) + 1);
+    new_name = malloc(len(name));
     if (new_name == NULL)
     {
-        return NULL;
+        return (NULL);
     }
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
     {
-        return NULL;
+        return (NULL);
     }
-    new_owner=malloc(strlen(owner) + 1);
+    new_owner=malloc(len(owner));
     if (new_owner == NULL)
     {
-        return NULL;
+        return (NULL);
     }   
-    strcpy(new_name, name);
+    ctr(new_name, name);
 
-    strcpy(new_owner, owner);
+    ctr(new_owner, owner);
 
     new_dog ->name = new_name;
     new_dog ->owner = new_owner;
     new_dog ->age = age;
-    return new_dog;
+    return (new_dog);
 }
