5a07b530195716d73a75ced949bce590c97dd666
aBDEL <abdel_azrhar@hotmail.com>
2025-07-16 23:41:27 +0200

    update file

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int sum = 0;
+va_start(args, n);
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+va_end(args);
+return sum;
+}
