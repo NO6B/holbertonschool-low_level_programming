1a6e58fc99da79e806e60b1c5b2843dffe7526c1
NO6B <abdel_azrhar@hotmail.com>
2025-07-16 12:10:05 +0200

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,10 +1,10 @@
-int int_index(int *array, int size, int (*cmp)(int))
 /**
  * int_index - function that searches for an integer
  * @array: array
  * @size: is the size of the array
  * @cmp: cmp is a pointer to the function to be used to compare values
  */
+int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 for (i = 0; i < size; i++)
