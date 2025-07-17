78f257cad79674afb4099569021fa605b60b69c7
aBDEL <abdel_azrhar@hotmail.com>
2025-07-17 17:08:10 +0200

    update

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,8 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
         {
             printf("%s", separator);
         }
-        else
-        return;
+        
     }
     va_end(args);
     printf("\n");
