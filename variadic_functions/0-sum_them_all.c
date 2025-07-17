ae2ef902ba86531246e40c709dce5653d7f13e12
NO6B <abdel_azrhar@hotmail.com>
2025-07-17 23:49:33 +0200

    Update 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,12 +9,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
-int sm = 0;
+int sum = 0;
 if (n == 0)
 return (0);
 va_start(args, n);
 for (i = 0; i < n; i++)
-sm += va_arg(args, int);
+sum += va_arg(args, int);
 va_end(args);
-return (sm);
+return (sum);
 }
