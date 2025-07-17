4455ee26616f27f528354410df766b464ee98f1a
NO6B <abdel_azrhar@hotmail.com>
2025-07-17 23:42:15 +0200

    Update 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,10 +11,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 unsigned int i;
 char *x;
 va_list args;
-va_start(args ,n);
+va_start(args, n);
 for (i = 0; i < n; i++)
 {
-x = va_arg(args , char *);
+x = va_arg(args, char *);
 printf("%s", x);
 if (separator != NULL && i < n - 1)
 {
