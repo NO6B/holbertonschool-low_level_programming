0da878e374be858b0a601ae10ab756a62cd1c14e
aBDEL <abdel_azrhar@hotmail.com>
2025-07-19 01:11:41 +0200

    ad

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -6,13 +6,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {0, 0}
+    op_t ops[] = {{"+", op_add},{"-", op_sub},{"*", op_mul},{"/", op_div},{"%", op_mod},{0, 0}
     };
 
     int i = 0;
