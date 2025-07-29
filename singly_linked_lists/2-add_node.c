42cb8fec51210eb2fe1a8387b4fad385681d5140
aBDEL <abdel_azrhar@hotmail.com>
2025-07-29 18:05:19 +0200

    creat function str len and strdup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 /**
  * add_node - Adds a new node at the beginning of a list_t list
@@ -7,6 +6,8 @@
  * @str: String to duplicate and store in the new node
  * Return: Address of the new node, or NULL on failure
  */
+char *my_strdup(const char *str);
+int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
@@ -17,14 +18,50 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     }
     
-    new_node ->str = strdup(str);
+    new_node ->str = my_strdup(str);
     if (new_node -> str == NULL)
     {
         free(new_node);
         return (NULL);
     }
-    new_node->len =strlen(str);
+    new_node->len =_strlen(str);
     new_node->next = *head;
     *head = new_node;
     return (new_node);
+}
+
+int _strlen(const char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
+
+
+char *my_strdup(const char *str)
+{
+    int len;
+    int i;
+    char *copy;
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    
+
+    len =_strlen(str);
+
+    copy= malloc(sizeof(char)* (len + 1));
+    if (copy == NULL)
+    {
+        return NULL;
+    }
+    
+    for (i = 0; i < len; i++)
+        copy[i] = str [i];
+    copy [i] = '\0';
+    return (copy);
 }
