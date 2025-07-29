4216f4e375bdc62d3cd96a875cd6784a13cde1c2
abdel <abdel_azrhar@hotmail.com>
2025-07-29 16:58:09 +0200

    update betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,17 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t elements = 0;
-    while (h != NULL)
-    {
-        if (h -> str == NULL)
-        {
-        printf("[0] (nil)\n");
-        }
-        else
-        printf("[%u] %s\n", h->len, h->str);
-        h = h -> next;
-        elements ++;
-    }
-    return (elements);
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+	{
+	printf("[0] (nil)\n");
+	}
+	else
+	printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+	elements++;
+	}
+
+	return (elements);
 }
