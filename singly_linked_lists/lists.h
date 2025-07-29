c10a56f9a0d2ccab26fa03140f18c04e3047242d
aBDEL <abdel_azrhar@hotmail.com>
2025-07-29 14:16:11 +0200

    update

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,3 +16,4 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
