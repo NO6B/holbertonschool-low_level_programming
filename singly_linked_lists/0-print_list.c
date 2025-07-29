3b0fb080707ac5050cf7e1de5e1deb39949121f8
NO6B <abdel_azrhar@hotmail.com>
2025-07-29 12:45:29 +0200

    Update 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
         printf("[%u] %s\n", h->len, h->str);
 
         h = h -> next;
-        (elements) ++;
+        elements ++;
     }
     return (elements) ;
 }
