4279c301e088926fbc7d3d995c56bce82e2000fc
abdel <abdel_azrhar@hotmail.com>
2025-07-30 17:01:12 +0200

    update

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
-
+#include <stdio.h>
 #include <stdlib.h>
 size_t print_dlistint(const dlistint_t *h)
 {
     int count = 0;
     while (h != NULL)
     {
-        printf("%d", h->n);
+        printf("%d\n", h->n);
         h = h->next;
         count++;
     }
