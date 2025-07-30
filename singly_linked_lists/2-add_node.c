96ba2aadc581a96be53114b6b0f4e844ce01a7d8
abdel <abdel_azrhar@hotmail.com>
2025-07-30 17:59:00 +0200

    betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,7 +2,6 @@
 #include "lists.h"
 /**
  * add_node - Adds a new node at the beginning of a list_t list
- * @head: Pointer to the head pointer
  * @str: String to duplicate and store in the new node
  * Return: Address of the new node, or NULL on failure
  */
@@ -10,58 +9,67 @@ char *my_strdup(const char *str);
 int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new_node;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-    
-    new_node ->str = my_strdup(str);
-    if (new_node -> str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->len =_strlen(str);
-    new_node->next = *head;
-    *head = new_node;
-    return (new_node);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+
+	new_node->str = my_strdup(str);
+	if (new_node->str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
-
+/**
+ * _strlen - Get length of a string
+ * @s: String to measure
+ * Return: Length of the string
+ */
 int _strlen(const char *s)
 {
-    int i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	return (i);
 }
 
-
+/**
+ * my_strdup - Duplicate a string
+ * @str: String to duplicate
+ * Return: Pointer to new string, or NULL if fail
+ */
 char *my_strdup(const char *str)
 {
-    int len;
-    int i;
-    char *copy;
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    
+	int len;
+	int i;
+	char *copy;
 
-    len =_strlen(str);
+	if (str == NULL)
+	{
+	return (NULL);
+	}
 
-    copy= malloc(sizeof(char)* (len + 1));
-    if (copy == NULL)
-    {
-        return NULL;
-    }
-    
-    for (i = 0; i < len; i++)
-        copy[i] = str [i];
-    copy [i] = '\0';
-    return (copy);
+	len = _strlen(str);
+
+	copy = malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+	return (copy);
 }
+
