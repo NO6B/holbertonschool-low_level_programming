fde2d7deaeec8576dfcee8b72534d5b17d4479ae
NO6B <abdel_azrhar@hotmail.com>
2025-07-31 13:33:40 +0200

    fix betty

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,12 +6,12 @@
  */ 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
-    while (head != NULL)
-    {
-    tmp = head->next;
+	dlistint_t *tmp;
+	while (head != NULL)
+	{
+	tmp = head->next;
 	free(head);
 	head = tmp;
-    }
-    
+	}
+
 }
