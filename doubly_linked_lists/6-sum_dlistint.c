61355723f1d31978ff237180e88e5b3b0c688516
NO6B <abdel_azrhar@hotmail.com>
2025-07-31 14:45:11 +0200

    Update 6-sum_dlistint.c

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * get_dnodeint_at_index - returns the nth node of a doubly linked list
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list.
  * @head: pointer to the head of the list
  * Return: 0 if head = NULL  else return i
  */
