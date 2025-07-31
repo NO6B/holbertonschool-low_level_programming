58e5b05b46717336ee228e557ca4a6e7b23c1ef4
NO6B <abdel_azrhar@hotmail.com>
2025-07-31 13:34:29 +0200

    Update 4-free_dlistint.c betty

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -3,10 +3,11 @@
 /**
  * free_dlistint - frees a dlistint_t list.
  * @head: Pointer to the head of the list
- */ 
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
+	
 	while (head != NULL)
 	{
 	tmp = head->next;
