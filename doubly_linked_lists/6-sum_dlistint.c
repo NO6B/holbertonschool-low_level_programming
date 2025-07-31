39080ac7443f859543bf4ab8fffd2f6aa8c95bef
Abdel <abdel_azrhar@hotmail.com>
2025-07-31 14:42:05 +0200

    update

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_dnodeint_at_index - returns the nth node of a doubly linked list
+ * @head: pointer to the head of the list
+ * Return: 
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    int i =0;
+
+    if (head==NULL)
+    {
+        return (0);
+    }
+    
+    while (head != NULL)
+    {
+    i += head->n;
+    head = head->next;
+    }
+return(i);
+}
