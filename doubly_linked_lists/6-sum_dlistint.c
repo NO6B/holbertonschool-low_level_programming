fda06d080a840d4160a2ca5f2930e72142bf930b
NO6B <abdel_azrhar@hotmail.com>
2025-07-31 14:44:07 +0200

    Update 6-sum_dlistint.c

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,21 +3,21 @@
 /**
  * get_dnodeint_at_index - returns the nth node of a doubly linked list
  * @head: pointer to the head of the list
- * Return: 
+ * Return: 0 if head = NULL  else return i
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int i =0;
+	int i = 0;
 
-    if (head==NULL)
-    {
-        return (0);
-    }
-    
-    while (head != NULL)
-    {
-    i += head->n;
-    head = head->next;
-    }
-return(i);
+	if (head == NULL)
+	{
+	return (0);
+	}
+
+	while (head != NULL)
+	{
+	i += head->n;
+	head = head->next;
+	}
+	return(i);
 }
