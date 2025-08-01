13942c7c95d2ec851724314681191b767730d587
Abdel <abdel_azrhar@hotmail.com>
2025-08-01 21:58:11 +0200

    a

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    int w;
+    int len = 0;
+
+    if (filename == NULL)
+    {
+        return (0);
+    }
+
+    fd = open(filename, O_CREAT | O_WRONLY)
+    if (fd == -1)
+    {
+        return (-1);
+    }
+    
+    if (text_content != NULL)
+    {
+        while (text_content[len])
+        {
+            len++;
+        }
+        if (w == -1)
+        {
+            return (-1);
+        }
+        
+
+    }
+    close(fd);
+    return (1);
+}
