21dae1216a229aa9cff4f06a0675fc11df7cde40
abdel <abdel_azrhar@hotmail.com>
2025-08-01 22:11:25 +0200

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
         return (0);
     }
 
-    fd = open(filename, O_CREAT | O_WRONLY);
+    fd = open(filename, O_CREAT | O_WRONLY, 0600);
     if (fd == -1)
     {
         return (-1);
