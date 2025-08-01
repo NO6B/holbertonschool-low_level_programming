d36570beeb18f816b510da6ddfabe888e91c280f
abdel <abdel_azrhar@hotmail.com>
2025-08-01 19:29:27 +0200

    test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     buffer = malloc(sizeof(char) * letters);
     if (buffer == NULL)
     {
-        return (NULL);
+        return (0);
     }
 
     r = read(fd, buffer, letters);
