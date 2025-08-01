728fa5c10b0e8d4333e5cb8d6b04f85e0fbd649d
aBDEL <abdel_azrhar@hotmail.com>
2025-08-01 19:24:09 +0200

    1-test

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t r;
+    ssize_t w;
+    char *buffer;
+
+    if (filename == NULL)
+    {
+        return (0);
+    }
+    
+    fd = open(filename, O_RDONLY);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        return (NULL);
+    }
+
+    r = read(fd, buffer, letters);
+    if (r == -1)
+    {
+        return (0);
+    }
+    
+    w = write(STDOUT_FILENO, buffer, r);
+    if (w == -1)
+    {
+        return (0);
+    }
+    
+    close(fd);
+    
+    return (w);
+}
