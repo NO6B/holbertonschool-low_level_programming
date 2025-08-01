516e406b26dd8750653845c24e242095077b24f8
abdel <abdel_azrhar@hotmail.com>
2025-08-01 19:33:42 +0200

    betty fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,39 +2,46 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+/**
+ * read_textfile - function that reads a text file and
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ * Return: Actual number of letters
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t r;
-    ssize_t w;
-    char *buffer;
+	int fd;
+	ssize_t r;
+	ssize_t w;
+	char *buffer;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    
-    fd = open(filename, O_RDONLY);
+	if (filename == NULL)
+	{
+	return (0);
+	}
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
 
-    r = read(fd, buffer, letters);
-    if (r == -1)
-    {
-        return (0);
-    }
-    
-    w = write(STDOUT_FILENO, buffer, r);
-    if (w == -1)
-    {
-        return (0);
-    }
-    
-    close(fd);
-    
-    return (w);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+	return (0);
+	}
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+	return (0);
+	}
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1)
+	{
+	return (0);
+	}
+
+	close(fd);
+
+	return (w);
 }
+
