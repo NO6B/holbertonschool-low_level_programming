560dea088cd3fb76e16e90f6c09518a4597df63c
abdel <abdel_azrhar@hotmail.com>
2025-08-03 16:16:38 +0200

    fix betty all files

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,21 +1,26 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: size is the size of the array
+ * Return: hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-    {
-        return (NULL);
-    }
-    
-    ht->size = size;
-    
-    ht->array = malloc(sizeof(ht->array)*size);
-    if (ht->array == NULL)
-    {
-        return (NULL);
-    }
-    return (ht);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+	return (NULL);
+	}
+
+	ht->size = size;
+
+	ht->array = malloc(sizeof(ht->array) * size);
+	if (ht->array == NULL)
+	{
+	return (NULL);
+	}
+	return (ht);
 }
+
--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
+/**
+ * key_index - function that gives you the index of a key.
+ * @size: size
+ * @key:the key
+ * Return: the index at which the key/value
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
+
